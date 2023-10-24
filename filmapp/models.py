fbe00abe8be27089e06bd461f2ebca5fa2935674
JayVicky <victorsasudje@gmail.com>
2023-10-24 08:14:27 +0100

    commit #6

--- filmapp/models.py
+++ filmapp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to='blog_images/blog_images', blank=True, null=True)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
